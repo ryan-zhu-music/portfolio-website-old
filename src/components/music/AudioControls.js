c6bcac44f95e1aa240e2743c67e67076bde6ddac
RZ-Music <ryanzhu2005@gmail.com>
2022-09-16 17:04:32 -0400

    Fix styling

--- src/components/music/AudioControls.js
+++ src/components/music/AudioControls.js
@@ -16,40 +16,36 @@ const AudioControls = ({
   width,
 }) => (
   <div className="track-audio-controls">
-    <button
-      type="button"
-      className="prev"
-      aria-label="Previous"
-      onClick={onPrevClick}
-    >
-      <FontAwesomeIcon icon={faBackwardStep} size="2x" />
+    <button type="button" aria-label="Previous" onClick={onPrevClick}>
+      <FontAwesomeIcon
+        icon={faBackwardStep}
+        size="2x"
+        style={{ fontSize: 25 }}
+      />
     </button>
     {isPlaying ? (
       <button
         type="button"
-        className="pause"
         onClick={() => onPlayPauseClick(false)}
         aria-label="Pause"
       >
-        <FontAwesomeIcon icon={faPause} size="2x" />
+        <FontAwesomeIcon icon={faPause} size="2x" style={{ fontSize: 25 }} />
       </button>
     ) : (
       <button
         type="button"
-        className="play"
         onClick={() => onPlayPauseClick(true)}
         aria-label="Play"
       >
-        <FontAwesomeIcon icon={faPlay} size="2x" />
+        <FontAwesomeIcon icon={faPlay} size="2x" style={{ fontSize: 25 }} />
       </button>
     )}
-    <button
-      type="button"
-      className="next"
-      aria-label="Next"
-      onClick={onNextClick}
-    >
-      <FontAwesomeIcon icon={faForwardStep} size="2x" />
+    <button type="button" aria-label="Next" onClick={onNextClick}>
+      <FontAwesomeIcon
+        icon={faForwardStep}
+        size="2x"
+        style={{ fontSize: 25 }}
+      />
     </button>
   </div>
 );
